74eeb4585fdb67d80362f4fd4d1f936539cb86d5
Haoran <haoranz5@andrew.cmu.edu>
2026-01-28 15:19:04 -0600

    stop tracking old files

--- exp_glue_fedlora2_wo_rotation_device1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# share_AB without rotation, use device 1
-python federatedscope/main.py --cfg federatedscope/glue/yamls/g_localstep20_fedlora2_shareAB_wo_rotation.yaml
-python federatedscope/main.py --cfg federatedscope/glue/yamls/g_localstep20_fedlora2_shareAB_wo_rotation_lr5e-3.yaml
-python federatedscope/main.py --cfg federatedscope/glue/yamls/global_fedlora2_shareAB_wo_rotation.yaml
-python federatedscope/main.py --cfg federatedscope/glue/yamls/global_fedlora2_shareAB_wo_rotation_lr5e-3.yaml
